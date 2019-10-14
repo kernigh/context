f1e010e88a676e2f77046f5e7da6ae8e90bead3b
George Koehler <kernigh@gmail.com>
2019-10-14 19:30:13 -0400

    2nd draft of ppc32 changes
    This draft removes the .eh_frame from ontop*.S and adds tail*.cpp, so
    the C++ compiler will emit the .eh_frame or equivalent.
    
    Might fix https://github.com/boostorg/context/issues/120

---
+++ src/asm/tail_ppc32_sysv_elf_gas.cpp
@@ -0,0 +1,17 @@
+
+//          Copyright Oliver Kowalke 2009.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//        http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/context/detail/fcontext.hpp>
+
+using boost::context::detail::transfer_t;
+
+// This C++ tail of ontop_fcontext() allocates transfer_t{ from, vp }
+// on the stack, because the ABI of fn() takes a transfer_t pointer.
+// If fn() throws a C++ exception, this tail removes its stack frame.
+extern "C" transfer_t
+ontop_fcontext_tail( int ignore, void * vp, transfer_t (* fn)(transfer_t), fcontext_t const from) {
+    return fn( transfer_t{ from, vp });
+}
